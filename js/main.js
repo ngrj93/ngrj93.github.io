3f135ae9a0402b2b6c126640219d1d284d71e94e
Nagaraj Poti <ngrj93@gmail.com>
2017-01-16 20:18:20 +0530

    Nagaraj Website
    A practice exposition of CSS, JS, HTML skills!

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+function changeColour() {
+    document.getElementById("button").style.color = "white";
+}
+
+function changeColourBack() {
+    document.getElementById("button").style.color = "#8CA93E";
+}
